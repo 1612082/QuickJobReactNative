c47a66856f1e0f847ad8ac2ea3441f94f7e4a7e2
1612041_phbinh <1612041_phbinh>
2020-07-09 04:19:43 +0700

    call api login

--- src/Authenticate/Login/Login.js
+++ src/Authenticate/Login/Login.js
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
-import { Text, StyleSheet, View, TouchableOpacity, Linking  } from "react-native";
+import AsyncStorage from '@react-native-community/async-storage';
+import { Text, StyleSheet, View, TouchableOpacity, Linking, KeyboardAvoidingView } from "react-native";
 
 import TextField from "../../Common/TextField";
 import { linkForgotPass } from "../../Global/string";
 import Logo from "../../Common/Logo";
 import ButtonFormat from "../../Common/Button";
-
+import loginRequest from "../../apiAuth/tokenClient";
 
 export default class Login extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      email: "",
+      password: "",
+    }
     this.TouchSignup = this.TouchSignup.bind(this);
     this.TouchLogin = this.TouchLogin.bind(this);
   }
@@ -17,26 +22,73 @@ export default class Login extends Component {
     this.props.navigation.navigate("Signup1");
   }
   TouchLogin() {
-    this.props.navigation.navigate("Home");
+    const client = loginRequest;
+    client
+      .post('/login', {
+        email: this.state.email,
+        password: this.state.password
+      })
+      .then(res => {
+        if (res.data.code === "-101") {
+          // thất bại
+          Alert.alert(res.data.message);
+        } else {
+          // thành công
+          AsyncStorage.setItem("token", JSON.stringify(res.data.data.token));
+          AsyncStorage.setItem("email", email);
+          this.props.navigation.navigate("Home");
+          // lấy thông tin user
+          // getUserInfo().then(res => {
+          //   if (res.data.code === '200') {
+          //     dispatch(updateUser(res.data.data.personal))
+          //     dispatch(success(res.data.message));
+          //     this.props.navigation.navigate("Home");
+          //   }
+          //   else {
+          //     Alert.alert('Lấy dữ liệu cá nhân thất bại.');
+          //   }
+          // }).catch(err => {
+          //   console.log(err);
+          // })
+        }
+
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Logo></Logo>
-        <TextField placeholder={" Email"}></TextField>
-        <TextField placeholder={" Mật khẩu"} pass={true}></TextField>
-        <ButtonFormat
-          content={"ĐĂNG NHẬP"}
-          TouchInside={this.TouchLogin}
-        ></ButtonFormat>
-        <ButtonFormat
-          content={"ĐĂNG KÝ"}
-          TouchInside={this.TouchSignup}
-        ></ButtonFormat>
-        <TouchableOpacity style={styles.touch} onPress={ ()=>{ Linking.openURL(linkForgotPass)}}>
-          <Text style={styles.underline}>Quên mật khẩu?</Text>
-        </TouchableOpacity>
+        <KeyboardAvoidingView behavior="position">
+          <Logo></Logo>
+          <TextField
+            placeholder={" Email"}
+            onChangeText={(text) => {
+              this.setState({ email: text });
+            }}
+          ></TextField>
+          <TextField
+            placeholder={" Mật khẩu"}
+            pass={true}
+            onChangeText={(text) => {
+              this.setState({ password: text });
+            }}
+          ></TextField>
+          <ButtonFormat
+            content={"ĐĂNG NHẬP"}
+            TouchInside={this.TouchLogin}
+          ></ButtonFormat>
+          <ButtonFormat
+            content={"ĐĂNG KÝ"}
+            TouchInside={this.TouchSignup}
+          ></ButtonFormat>
+          <TouchableOpacity style={styles.touch} onPress={() => { Linking.openURL(linkForgotPass) }}>
+            <Text style={styles.underline}>Quên mật khẩu?</Text>
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
+
       </View>
     );
   }
--- src/Common/TextField.js
+++ src/Common/TextField.js
@@ -4,7 +4,7 @@ import { StyleSheet, TextInput, View } from 'react-native'
 const TextField = (props) => {
     return (
         <View>
-            <TextInput keyboardType={props.typekeyboard} secureTextEntry = {props.pass} placeholder = {props.placeholder} placeholderTextColor = 'black' style={styles.text}></TextInput>
+            <TextInput onChangeText={(text) => props.onChangeText(text)} keyboardType={props.typekeyboard} secureTextEntry={props.pass} placeholder={props.placeholder} placeholderTextColor='black' style={styles.text}></TextInput>
         </View>
     )
 }
@@ -12,10 +12,10 @@ const TextField = (props) => {
 export default TextField
 
 const styles = StyleSheet.create({
-    text:{
+    text: {
         height: 40,
         borderColor: 'gray',
-        borderWidth:1,
+        borderWidth: 1,
         width: '80%',
         alignSelf: 'center',
         borderRadius: 15,
